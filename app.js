017db09776e8529b4a8c39ad42f3b8d6ddec92b4
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-10-30 21:14:23 +0200

    simple data types and arr

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+// первый пример 
 var button = document.querySelector('#btn');
 var input1 = document.querySelector('#num-1');
 var input2 = document.querySelector('#num-2');
-console.log(button);
 function add(a, b) {
     return a + b;
 }
@@ -9,3 +9,26 @@ function add(a, b) {
 button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+// простые типы данных 
+var num;
+num = 5;
+var str;
+str = "string";
+var person = "Maxim"; // можно не опредилять тип данных
+// сложные типы данных
+// массив строк 
+var arrString;
+arrString = ["effgg", "max", "ilon"];
+console.log(arrString);
+// массив чисел
+var arrNumber;
+// для всех типов даных
+var arrAny;
+// для массивов обьектов
+var arrObject;
+arrObject = [{ name: "Vill" }];
+// тип object
+var obj = {}; //const obj: {} = {};
+// Для не обязательных полей есть оператор ?
+var obj1 = {};
+obj1.name = 'Alex';
